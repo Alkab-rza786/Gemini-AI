3c3e43dd12ab47e73fdbfaf6058db8311c38a8be
Alkab-rza786 <153321032+Alkab-rza786@users.noreply.github.com>
2024-07-09 20:07:00 +0530

    first commit
    - **Developed** a website utilizing Gemini AI API to **provide** real-time answers to user queries
    - **Implemented** AI-driven responses, **increasing** user engagement by 30%
    - **Integrated** advanced AI technology, **enhancing** user experience with 95% accuracy in responses
    - **Streamlined** the user interface for seamless interaction, **reducing** response time by 20%

---
+++ Gemini-App/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SideBar from './Components/SideBar/SideBar'
+import Main from './Components/Main/Main'
+
+function App() {
+  return (
+    <>
+      <SideBar />
+      <Main />
+    </>
+
+  )
+}
+
+export default App
